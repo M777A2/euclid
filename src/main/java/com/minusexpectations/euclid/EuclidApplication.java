341434593e0e04e3d5bb1ce7742d7df2d925e58c
Zephan The <zephan.the@gmail.com>
2018-05-27 19:25:40 -0700

    init commit

---
+++ src/main/java/com/minusexpectations/euclid/EuclidApplication.java
@@ -0,0 +1,12 @@
+package com.minusexpectations.euclid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuclidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuclidApplication.class, args);
+	}
+}
