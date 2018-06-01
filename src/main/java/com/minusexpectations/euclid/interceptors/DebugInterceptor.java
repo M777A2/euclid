86a48290826031abe608149870da29cf3ec20595
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2018-06-01 11:26:14 -0700

    Added a debug log for unsuccessful requests. Updated jQuery.

--- src/main/java/com/minusexpectations/euclid/interceptors/DebugInterceptor.java
+++ src/main/java/com/minusexpectations/euclid/interceptors/DebugInterceptor.java
@@ -2,6 +2,7 @@ package com.minusexpectations.euclid.interceptors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,5 +24,27 @@ public class DebugInterceptor extends HandlerInterceptorAdapter
     {
         log.trace("Received {} request for URI: {}", request.getMethod(), request.getRequestURI());
         return super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler,
+            ModelAndView modelAndView)
+            throws Exception
+    {
+        int status = response.getStatus() / 100;
+
+
+        if (status != 2)
+        {
+            log.debug("{} request to url \"{}\" failed with status {}",
+                    request.getMethod(),
+                    request.getRequestURI(),
+                    response.getStatus());
+        }
+
+        super.postHandle(request, response, handler, modelAndView);
     }
 }
--- src/main/java/com/minusexpectations/euclid/security/SecurityConfig.java
+++ src/main/java/com/minusexpectations/euclid/security/SecurityConfig.java
@@ -25,29 +25,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     protected void configure(HttpSecurity http) throws Exception
     {
         http
-                .authorizeRequests()
-                    .antMatchers(
-                            "/",
-                            "/js/**",
-                            "/css/**",
-                            "/img/**",
-                            "/webjars/**").permitAll()
-                    .antMatchers("/user/**").hasRole("USER")
-                    .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                .and()
-                .logout()
-                    .invalidateHttpSession(true)
-                    .clearAuthentication(true)
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("/login?logout")
-                    .permitAll()
-                .and()
-                .exceptionHandling()
-                    .accessDeniedHandler(accessDeniedHandler);
+            .authorizeRequests()
+                .antMatchers(
+                        "/",
+                        "/js/**",
+                        "/css/**",
+                        "/img/**",
+                        "/webjars/**").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .anyRequest().authenticated()
+            .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+            .and()
+            .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll()
+            .and()
+            .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler);
     }
 
     /**
